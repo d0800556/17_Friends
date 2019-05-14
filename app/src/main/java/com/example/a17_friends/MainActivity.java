41af095b94dbc60f927550de2eb604fc607348cd
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-05-14 20:41:04 +0800

    製作中

--- app/src/main/java/com/example/a17_friends/MainActivity.java
+++ app/src/main/java/com/example/a17_friends/MainActivity.java
@@ -2,8 +2,11 @@ package com.example.a17_friends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Toolbar mToolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
