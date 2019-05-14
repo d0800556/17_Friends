9d793c4ebaa27706eb2062a6b1f833f9fac2acb8
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-05-14 20:46:04 +0800

    製作中2

--- app/src/main/java/com/example/a17_friends/MainActivity.java
+++ app/src/main/java/com/example/a17_friends/MainActivity.java
@@ -2,7 +2,7 @@ package com.example.a17_friends;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.Toolbar;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,5 +12,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mToolbar = (Toolbar) findViewById(R.id.main_page_toolbar);
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("17-Friends");
     }
 }
