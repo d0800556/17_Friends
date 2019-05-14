96e7dc0dbcf3b6893c1e4129c7381c92643d0ae8
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-05-14 23:17:17 +0800

    第11章

--- app/src/main/java/com/example/a17_friends/MainActivity.java
+++ app/src/main/java/com/example/a17_friends/MainActivity.java
@@ -6,6 +6,8 @@ import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import com.google.firebase.auth.FirebaseUser;
 
@@ -49,5 +51,35 @@ public class MainActivity extends AppCompatActivity {
 
         Intent loginIntent = new Intent (MainActivity.this, LoginActivity.class);
         startActivity(loginIntent);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.options_menu,menu);
+        return true;
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        super.onContextItemSelected(item);
+
+        if(item.getItemId() == R.id.main_logout_option)
+        {
+            mAuth.signout();
+            SendUserToLoginActivity();
+        }
+
+        if(item.getItemId() == R.id.main_setting_option)
+        {
+
+        }
+
+        if(item.getItemId() == R.id.main_find_friends_option)
+        {
+
+        }
+
+        return true;
     }
 }
