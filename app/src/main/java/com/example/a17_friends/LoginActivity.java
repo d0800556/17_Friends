a3b1dbba7eb31856c6b1de628bf7de6891dd7b64
jiamax <zz12345zxc@gmail.com>
2019-05-14 21:53:06 +0800

    第六章 新增:登入,註冊 xml:buttons inputs

--- app/src/main/java/com/example/a17_friends/LoginActivity.java
+++ app/src/main/java/com/example/a17_friends/LoginActivity.java
@@ -9,5 +9,22 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+
+    }
+
+    protected  void onStart()
+    {
+        super.onStart();
+        if (currentUser = null)
+        {
+            SendUserToMainActivity();
+        }
+    }
+
+    private void SendUserToMainActivity() {
+
+        Intent loginIntent = new Intent(LoginActivity.this, MainActivity.this)
+                startActivity(loginIntent)
     }
 }
