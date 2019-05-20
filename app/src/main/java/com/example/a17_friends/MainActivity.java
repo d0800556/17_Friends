0424a712968f553580a92bef2b9e0938ce7d7c3b
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-05-20 22:02:24 +0800

    第16章

--- app/src/main/java/com/example/a17_friends/MainActivity.java
+++ app/src/main/java/com/example/a17_friends/MainActivity.java
@@ -1,16 +1,22 @@
 package com.example.a17_friends;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.text.TextUtils;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.EditText;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.database.DataSnapshot;
@@ -112,12 +118,69 @@ public class MainActivity extends AppCompatActivity {
             SendUserToSettingActivity();
         }
 
+        if(item.getItemId() == R.id.main_create_group)
+        {
+            RequestNewGroup();
+        }
+
         if(item.getItemId() == R.id.main_find_friends_option)
         {
 
         }
 
         return true;
+    }
+
+    private void RequestNewGroup()
+    {
+        AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this,R.style.AlertDialog);
+        builder.setTitle("群組名稱:");
+
+        final EditText groupNameField = new EditText(MainActivity.this);
+        groupNameField.setHint("17-Friends");
+        builder.setView(groupNameField);
+
+        builder.setPositiveButton("創建", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String groupname = groupNameField.getText().toString();
+
+                if (TextUtils.isEmpty(groupname))
+                {
+                    Toast.makeText(MainActivity.this, "請輸入群組名稱...", Toast.LENGTH_SHORT).show();
+                }
+                else
+                {
+                    CreateNewGroup(groupname);
+
+                }
+            }
+        });
+
+        builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+
+                dialog.cancel();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void CreateNewGroup(final String groupname)
+    {
+        RootRef.child("Groups").child(groupname).setValue("")
+                .addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task)
+                    {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(MainActivity.this, groupname + "群組創建成功!!", Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
     }
 
     private void SendUserToLoginActivity() {
