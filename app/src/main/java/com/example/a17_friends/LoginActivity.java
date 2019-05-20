be558e951144f25e9eaf7e2fb864ac0c2a59807c
PG One <love3131love@yahoo.com.tw>
2019-05-21 04:22:49 +0800

    第23章 完成

--- app/src/main/java/com/example/a17_friends/LoginActivity.java
+++ app/src/main/java/com/example/a17_friends/LoginActivity.java
@@ -57,6 +57,13 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        PhoneLoginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent phoneLoginIntent = new Intent(LoginActivity.this, PhoneLoginActivity.class);
+                startActivity(phoneLoginIntent);
+            }
+        });
 
     }
 
