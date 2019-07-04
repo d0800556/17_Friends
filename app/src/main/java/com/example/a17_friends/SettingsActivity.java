f7d935c66927597ec4ee7de7f5da44a493ed04b0
d0800556 <50147650+d0800556@users.noreply.github.com>
2019-07-05 01:55:46 +0800

    第42 43章

--- app/src/main/java/com/example/a17_friends/SettingsActivity.java
+++ app/src/main/java/com/example/a17_friends/SettingsActivity.java
@@ -7,6 +7,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
@@ -46,6 +47,7 @@ public class SettingsActivity extends AppCompatActivity {
     private static  final int GalleryPick = 1;
     private StorageReference UserProfileImagesRef;
     private ProgressDialog loadingBar;
+    private Toolbar SettingsToolBar;
 
     @Override
     protected void onCreate(final Bundle savedInstanceState) {
@@ -89,6 +91,12 @@ public class SettingsActivity extends AppCompatActivity {
         userName = (EditText) findViewById(R.id.set_user_name);
         userStatus = (EditText) findViewById(R.id.set_profile_status);
         userProfileImage = (CircleImageView) findViewById(R.id.set_profile_image);
+
+        SettingsToolBar = (Toolbar) findViewById(R.id.settings_toolbar);
+        setSupportActionBar(SettingsToolBar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        getSupportActionBar().setDisplayShowCustomEnabled(true);
+        getSupportActionBar().setTitle("帳戶設定");
 
     }
 
@@ -184,11 +192,11 @@ public class SettingsActivity extends AppCompatActivity {
         }
         else
         {
-            HashMap<String,String> profileMap = new HashMap<>();
+            HashMap<String,Object> profileMap = new HashMap<>();
                 profileMap.put("uid",currentUserID);
                 profileMap.put("name",setUserName);
                 profileMap.put("status",setStatus);
-             RootRef.child("Users").child(currentUserID).setValue(profileMap)
+             RootRef.child("Users").child(currentUserID).updateChildren(profileMap)
                      .addOnCompleteListener(new OnCompleteListener<Void>() {
                          @Override
                          public void onComplete(@NonNull Task<Void> task) {
