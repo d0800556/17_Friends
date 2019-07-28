a8bdf7f2391f67ff16f1931b8e39099f7faf4289
jiamax <zz12345zxc@gmail.com>
2019-07-29 00:36:49 +0800

    新增 1.註冊帳號動畫切換 2.忘記密碼功能

--- app/src/main/java/com/example/a17_friends/LoginActivity.java
+++ app/src/main/java/com/example/a17_friends/LoginActivity.java
@@ -1,21 +1,27 @@
 package com.example.a17_friends;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.sax.TextElementListener;
 import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.telephony.mbms.MbmsErrors;
+import android.text.InputType;
 import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import com.firebase.ui.database.FirebaseIndexArray;
 import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
@@ -42,8 +48,6 @@ public class LoginActivity extends AppCompatActivity {
         setContentView(R.layout.activity_login);
         mAuth = FirebaseAuth.getInstance();
         UsersRef = FirebaseDatabase.getInstance().getReference().child("Users");
-
-
         RootRef = FirebaseDatabase.getInstance().getReference();
         currentUser = mAuth.getCurrentUser();
         InitializeFields();
@@ -53,6 +57,7 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view)
             {
                 SendUserToRegisterActivity();
+
             }
         });
 
@@ -71,6 +76,75 @@ public class LoginActivity extends AppCompatActivity {
                 startActivity(phoneLoginIntent);
             }
         });
+
+        ForgetPasswordLink.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                forgetpassword();
+                }
+
+
+        });
+
+    }
+
+
+    private void forgetpassword() {  //
+        final AlertDialog.Builder builder= new AlertDialog.Builder(this);
+        builder.setTitle("忘記密碼");
+        LinearLayout linearLayout = new LinearLayout(this);
+        final EditText emailEt= new EditText(this);
+        emailEt.setHint("輸入Email");
+        emailEt.setInputType(InputType.TYPE_TEXT_VARIATION_EMAIL_ADDRESS);
+        emailEt.setMinEms(20);
+        linearLayout.addView(emailEt);
+        linearLayout.setPadding(20,20,20,20);
+        builder.setView(linearLayout);
+
+        builder.setPositiveButton("送出", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                String email = emailEt.getText().toString().trim();
+                FirebaseAuth mAuth = FirebaseAuth.getInstance();
+                final ProgressDialog progressDialog = new ProgressDialog(LoginActivity.this);
+                progressDialog.setMessage("驗證中..");
+                progressDialog.show();
+
+                mAuth.sendPasswordResetEmail(email)
+                        .addOnCompleteListener(new OnCompleteListener<Void>() {
+                            @Override
+                            public void onComplete(@NonNull Task<Void> task) {
+                                if(task.isSuccessful()){
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getApplicationContext(), "密碼重設已寄出l",
+                                            Toast.LENGTH_SHORT).show();
+                                }else{
+                                    progressDialog.dismiss();
+                                    Toast.makeText(getApplicationContext(),
+                                            "Email 不存在", Toast.LENGTH_SHORT).show();
+                                }
+                            }
+                        }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        progressDialog.dismiss();
+                        Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+            }
+
+        });
+
+
+            builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                @Override
+                public void onClick(DialogInterface dialog, int which) {
+                    dialog.dismiss();
+                }
+            });
+
+        builder.create().show();
 
     }
 
@@ -132,6 +206,9 @@ public class LoginActivity extends AppCompatActivity {
     private void SendUserToRegisterActivity() {
         Intent registerIntent = new Intent(LoginActivity.this, RegisterActivity.class);
         startActivity(registerIntent);
+        overridePendingTransition(R.anim.slide_in_right,R.anim.stay);
+
+
     }
 
     protected  void onStart()
